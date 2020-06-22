1ac1b71f15e3e3ab1425ecc126c0f146cb881791
Renato Groffe <renato.groffe@yahoo.com.br>
2020-06-22 10:28:03 -0300

    ASP.NET Core 3.1 + REST API + Serilog + MongoDB
    ASP.NET Core 3.1 + REST API + Serilog + MongoDB

---
+++ APIContagem/Controllers/ContadorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace APIContagem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContadorController : ControllerBase
+    {
+        private static Contador _CONTADOR = new Contador();
+        private readonly ILogger<ContadorController> _logger;
+
+        public ContadorController(ILogger<ContadorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get([FromServices]IConfiguration configuration)
+        {  
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+                _logger.LogInformation($"APIContagem - Valor Atual: {_CONTADOR.ValorAtual}");
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    Environment.MachineName,
+                    Local = "Teste",
+                    Sistema = Environment.OSVersion.VersionString,
+                    Variavel = configuration["TesteAmbiente"],
+                    TargetFramework = Assembly
+                        .GetEntryAssembly()?
+                        .GetCustomAttribute<TargetFrameworkAttribute>()?
+                        .FrameworkName
+                };
+            }
+        }
+    }
+}
---
+++ APIContagem/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace APIContagem
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                var hostBuilder = CreateHostBuilder(args).Build();
+                Serilog.Log.Information("Iniciando Web Host");
+                hostBuilder.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Fatal(ex, "Host encerrado inesperadamente");
+                return 1;
+            }
+            finally
+            {
+                Serilog.Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    Serilog.Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .WriteTo.MongoDB(settings.GetConnectionString("BaseLog"), collectionName: "LogAPIContagem")
+                        .CreateLogger();
+                })
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
